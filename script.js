4c5e299af57531139899e581cc66be7b95624a8a
Rasmus3liasson <ras.muse@hotmail.com>
2022-11-10 14:15:12 +0100

    works but need som fix to eliminate some js

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const hamburgerIcon = document.querySelector(".hamburger-icon");
-const navMenu = document.querySelector("nav");
 const closeIcon = document.createElement("img");
 
 closeIcon.classList.add("close-icon");
 closeIcon.src = "images/logotype-images/close-icon.png";
 
-hamburgerIcon.addEventListener("click", function () {
-  navMenu.classList.add("nav-menu-mobile");
-
-  navMenu.appendChild(closeIcon);
-});
+document
+  .querySelector(".hamburger-icon")
+  .addEventListener("click", function () {
+    document.querySelector("nav").classList.add("nav-menu-mobile");
+    document.querySelector("nav").appendChild(closeIcon);
+    document
+      .querySelector(".nav-menu-background")
+      .classList.add("background-opacity");
+    document
+      .querySelector(".nav-menu-background-one")
+      .classList.add("background-opacity");
+  });
 
 closeIcon.addEventListener("click", function () {
-  navMenu.classList.remove("nav-menu-mobile");
+  document.querySelector("nav").classList.remove("nav-menu-mobile");
+
+  document
+    .querySelector(".nav-menu-background")
+    .classList.remove("background-opacity");
+  document
+    .querySelector(".nav-menu-background-one")
+    .classList.remove("background-opacity");
 });
